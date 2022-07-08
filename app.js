abde52502b3c122f2a4d223e6c82200f42df920d
osmanforhad <developer.osmanforhad@gmail.com>
2022-07-08 23:46:49 +0600

    working with undefined route handaling

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const app = new express();
 
 app.use("/api/v1", router);
 
+//Undefined Route
+app.use("*", (request, response) => {
+    response.status(404).json({status:"fail", data:"Not Found"});
+});
+
 module.exports = app;
